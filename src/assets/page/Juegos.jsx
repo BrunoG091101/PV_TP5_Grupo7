6c788d07db592de5dfb3f43d833da82f41b82f9a
AgustinaAyelenCordoba <165093267+AgustinaAyelenCordoba@users.noreply.github.com>
2025-10-23 17:58:00 -0300

    Juego Estrella
    Se agregaron las imágenes del fondo y la estrella, tambien se creo el archivo JuegoEstrella.jsx en la carpeta componentes-assets, se modifico App.css y se agrego archivo Juego.jsx en la carpeta page-assets

---
+++ src/assets/page/Juegos.jsx
@@ -0,0 +1,9 @@
+import JuegoEstrella from "../Components/JuegoEstrella"
+function Juegos(){
+    return(
+        <div>
+            <JuegoEstrella/>
+        </div>
+    )
+}
+export default Juegos;
